a8421b075808c8256f27dc9e91a3f55e8bbfda01
Jacob Adams <jacob@tookmund.com>
2020-10-26 22:28:04 +0000

    onefio: Run fio just once instead of pre/post

---
+++ onefio.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ -z $1 ]
+then
+	echo "Requires a Test File!"
+	exit 1
+fi
+
+NAME="$(basename "$1")"
+
+fio \
+	--readonly \
+	--ioengine=psync \
+	--rw=read \
+	--name "$NAME" \
+	--filename "$1" \
+	--output "fio.$NAME"
