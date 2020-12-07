901c3c2f395bf4b33b95c8baf94df9b2e763e3b0
Jacob Adams <jacob@tookmund.com>
2020-12-07 19:19:49 +0000

    autoarctest: Back to opposite node tests

--- autoarctest/make-test-run.sh
+++ autoarctest/make-test-run.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 
-for b in U
+
+for b in  O
 do
 	for n in 0 1
 	do
 		for t in fio
 		do
-			for f in /tank/2-1G
+			for f in /tank/*
 			do
-				i=0
-				while [ $i -ne 25 ]
-				do
-					touch "$b.$n.$t.$(basename "$f").$i"
-					i=$((i + 1))
-				done
+				touch "$b.$n.$t.$(basename "$f")"
 			done
 		done
 	done
