9b57eb5b7ec82eab8017d3c44ff070fb03f08d78
Jacob Adams <jacob@tookmund.com>
2020-12-12 23:35:20 +0000

    autoarctest opposite node: Only generate the data we need

--- autoarctest/make-test-run.sh
+++ autoarctest/make-test-run.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-
+#!/bin/bash
 
 for b in  O
 do
@@ -7,7 +6,7 @@ do
 	do
 		for t in fio
 		do
-			for f in /tank/*
+			for f in /tank/{0-12K,1-128K,500M,2-1G,2G,4G,8G,16G}
 			do
 				touch "$b.$n.$t.$(basename "$f")"
 			done
