7936036fafe3533b5674335285ab5f6af717d595
Jacob Adams <jaadams01@email.wm.edu>
2021-01-19 14:35:35 -0500

    getdata: Extract bandwidth before latency

--- getdata.sh
+++ getdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BIN="$(dirname "$0")"
-for s in $BIN/extract{50latency,bandwidth}.awk
+for s in $BIN/extract{bandwidth,50latency}.awk
 do
     for f in fio.{0-12K,1-128K,500M,2-1G,2G,4G,8G,16G}.$1
     do
