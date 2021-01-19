14ad85b21749c32c88bb88efc3f34e512b47039f
Jacob Adams <jaadams01@email.wm.edu>
2021-01-19 14:21:01 -0500

    getdata: extract bandwidth and latency and simplify usage

--- getdata.sh
+++ getdata.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
-for f in */*.post*
+#!/bin/bash
+BIN="$(dirname "$0")"
+for s in $BIN/extract{latency,bandwidth}.awk
 do
-	echo $f
-	../../../extractdata.awk "$f"
+    for f in fio.{0-12K,1-128K,500M,2-1G,2G,4G,8G,16G}.$1
+    do
+
+        echo $f
+        $s $f
+    done
+    printf "\n\n"
 done
