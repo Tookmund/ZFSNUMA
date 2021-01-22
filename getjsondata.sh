556d863685b211dd600f41565c4d5d8d20286376
Jacob Adams <jaadams01@email.wm.edu>
2021-01-22 12:01:22 -0500

    getjsondata: json data has no suffix thanks to preread

--- getjsondata.sh
+++ getjsondata.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 BIN="$(dirname "$0")"
-for f in fio.{0-12K,1-128K,500M,2-1G,2G,4G,8G,16G}.$1
+for f in fio.{0-12K,1-128K,500M,2-1G,2G,4G,8G,16G}
 do
 	echo $f
 	"$BIN/extractfiojson.py" $f
+	printf "\n"
 done
 printf "\n\n"
