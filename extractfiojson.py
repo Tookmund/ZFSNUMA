1ede49a83c7c8998d32479e4a847af87dc2f35ac
Jacob Adams <jaadams01@email.wm.edu>
2021-01-28 09:48:16 -0500

    extractfiojson, getjsondata: Make extracting fio data more foolproof

--- extractfiojson.py
+++ extractfiojson.py
@@ -3,9 +3,15 @@
 import json
 import sys
 
-with open(sys.argv[1]) as f:
+stat = sys.argv[1]
+fname = sys.argv[2]
+with open(fname) as f:
     j = json.load(f)["jobs"][0]["read"]
-    print("Bandwidth:", j["bw_bytes"])
-    print("50% Latency:", j["clat_ns"]["percentile"]["50.000000"])
-    print("99% Latency:", j["clat_ns"]["percentile"]["99.000000"])
-    print("Average Latency:", j["lat_ns"]["mean"])
+    if stat == "bw":
+        print("Bandwidth:", j["bw_bytes"])
+    elif stat == "50lat" or stat == "50":
+        print("50% Latency:", j["clat_ns"]["percentile"]["50.000000"])
+    elif stat == "99lat" or stat == "99":
+        print("99% Latency:", j["clat_ns"]["percentile"]["99.000000"])
+    elif stat == "avglat" or stat == "avg":
+        print("Average Latency:", j["lat_ns"]["mean"])
--- getjsondata.sh
+++ getjsondata.sh
@@ -3,7 +3,7 @@ BIN="$(dirname "$0")"
 for f in fio.{0-12K,1-128K,500M,2-1G,2G,4G,8G,16G}
 do
 	echo $f
-	"$BIN/extractfiojson.py" $f
+	"$BIN/extractfiojson.py" "$1" "$f"
 	printf "\n"
 done
 printf "\n\n"
