a4fb85a060bed67dcd58f164adf67cc8d0bc2c3b
Jacob Adams <jaadams01@email.wm.edu>
2021-02-14 18:17:41 -0500

    more getdata scripts, doubling and small opposite

---
+++ getdoublingoppositedatajson.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+BIN="$(dirname "$0")"
+EXT=""
+if [ ! -z "${2+x}" ]
+then
+	EXT=".$2"
+fi
+for f in ./$3/fio.{250M,500M,2-1G,2G,4G,8G,16G}$EXT
+do
+	echo $f
+	"$BIN/extractfiojson.py" "$1" "$f"
+	printf "\n"
+done
+printf "\n\n"
