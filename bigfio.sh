16b10ccc485d164d584a92282c7bd51a1ef24ff7
Jacob Adams <jacob@tookmund.com>
2021-02-17 11:36:02 -0500

    fio with big and small file sizes

---
+++ bigfio.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ -z $1 ]
+then
+	echo "Requires a Test File!"
+	exit 1
+fi
+
+NAME="$(basename "$1")"
+
+for p in pre post
+do
+	fio \
+		--readonly \
+		--invalidate=0 \
+		--ioengine=psync \
+		--rw=read \
+		--name "$NAME" \
+		--filename "$1" \
+		--output-format=json \
+		--output "fio.$NAME.$p$2" \
+		--bs=1M
+done
