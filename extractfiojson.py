47a3d948b88add86f3ad6121e401da18bac016a7
Jacob Adams <jaadams01@email.wm.edu>
2021-02-18 14:46:03 -0500

    extractfiojson: Don't catch exceptions

--- extractfiojson.py
+++ extractfiojson.py
@@ -16,9 +16,6 @@ with open(fname) as f:
     elif stat == "avglat" or stat == "avg":
         print("Average Latency:", j["lat_ns"]["mean"])
     else:
-        try:
-            d = int(stat)
-            print("{}% Latency:".format(d),
-                j["clat_ns"]["percentile"]["{}.000000".format(d)])
-        except:
-            print("No Such Stat {}".format(stat))
+        d = int(stat)
+        print("{}% Latency:".format(d),
+            j["clat_ns"]["percentile"]["{}.000000".format(d)])
