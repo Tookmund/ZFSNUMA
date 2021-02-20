e68d584364ccd9e64dedfc261d3ce7762bdbac8f
Jacob Adams <jaadams01@email.wm.edu>
2021-02-20 16:51:49 -0500

    getdoublingoppositedatajson: Support big and small fio

--- getdoublingoppositedatajson.sh
+++ getdoublingoppositedatajson.sh
@@ -5,7 +5,7 @@ if [ ! -z "${2+x}" ]
 then
 	EXT=".$2"
 fi
-for f in ./$3/fio.{250M,500M,2-1G,2G,4G,8G,16G}$EXT
+for f in ./$3/$4fio.{250M,500M,2-1G,2G,4G,8G,16G}$EXT
 do
 	echo $f
 	"$BIN/extractfiojson.py" "$1" "$f"
