14ef2e8de4a65c83d0f1f282465b787f30705ab1
Jacob Adams <jaadams01@email.wm.edu>
2021-02-20 23:34:12 -0500

    getdata: Consolidate data-extraction scripts

--- getdata.sh
+++ getdata.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
+# getdata.sh <data format> <which data> <fio type> <what data> <folder> <suffix>
 BIN="$(dirname "$0")"
-for s in $BIN/extract{bandwidth,50latency}.awk
-do
-    for f in fio.{0-12K,1-128K,500M,2-1G,2G,4G,8G,16G}.$1
-    do
 
-        echo $f
-        $s $f
-    done
-    printf "\n\n"
+FORMAT="$1"
+WHICH="$2"
+FIOTYPE="$3"
+WHAT="$4"
+FOLDER="$5"
+SUFFIX="$6"
+
+if [ "$WHICH" = "legacy" ]
+then
+	WHICH="{0-12K,1-128K,500M,2-1G,2G,4G,8G,16G}"
+elif [ "$WHICH" = "doubling" ]
+then
+	WHICH="{250M,500M,2-1G,2G,4G,8G,16G}"
+fi
+
+if [ ! -z "${SUFFIX+x}" ]
+then
+	SUFFIX=".$SUFFIX"
+fi
+
+for f in $(eval echo ./${FOLDER}/${FIOTYPE}fio.${WHICH}${SUFFIX})
+do
+	echo $f
+	"$BIN/extractfio${FORMAT}.py" "$WHAT" "$f"
+	printf "\n"
 done
