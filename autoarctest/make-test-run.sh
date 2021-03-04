773388cd16d629ab57c9673bb9a14d36b29e2a98
Jacob Adams <jacob@tookmund.com>
2021-03-04 00:28:00 -0500

    make-test-run: Replace custom scripts per test with one script

---
+++ autoarctest/make-test-run.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+SCRIPT="$1"
+FILES="$2"
+
+case "$FILES" in
+	"doubling")
+		FILES="250M,500M,2-1G,2G,4G,8G,16G"
+		;;
+	"doubling4G")
+		FILES="250M,500M,2-1G,2G,4G"
+		;;
+	"small")
+		FILES="0-12K,1-128K,256K,512K,1M,10M,100M,250M,500M,750M,2-1G"
+		;;
+	"legacy")
+		FILES="0-12K,1-128K,500M,2-1G,2G,4G,8G,16G"
+		;;
+esac
+
+RUNS="$3"
+OPPOSITE="$4"
+BLOCKSIZE="$5"
+
+if [ -z "$BLOCKSIZE" ]
+then
+	BLOCKSIZE="-"
+fi
+
+for b in U B
+do
+	for n in 0 1
+	do
+		for t in $SCRIPT
+		do
+			for f in $(eval echo "/tank/{$FILES}")
+			do
+				for r in $(seq $RUNS)
+				do
+					for bs in $BLOCKSIZE
+					do
+						FN="$b.$n.$t.$(basename "$f").$r"
+						if [ ! -z "$OPPOSITE" ]
+						then
+							FN="$FN.O"
+						fi
+						if [ ! -z "$BLOCKSIZE" ] && [ ! "$BLOCKSIZE" = "-" ]
+						then
+							FN="$FN.$bs"
+						fi
+						touch "$FN"
+					done
+				done
+			done
+		done
+	done
+done
