370d5c7a9f9f0d13c74d1ed9c2ec497472cee419
Jacob Adams <jacob@tookmund.com>
2021-05-10 17:27:53 -0400

    randomfio: Add fio random read testing

---
+++ randomfio.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+if [ -z $1 ]
+then
+	echo "Requires a Test File!"
+	exit 41
+fi
+
+NAME="$(basename "$1")"
+BLOCKSIZE="4096"
+
+if [ ! -z $3 ]
+then
+	BLOCKSIZE="$3"
+fi
+
+for p in pre post
+do
+	FILENAME="$3fio.$NAME.$p$2"
+
+	if [ -f "$FILENAME" ]
+	then
+		echo "ERROR: $FILENAME exists!"
+		exit 42
+	fi
+	fio \
+		--readonly \
+		--invalidate=0 \
+		--ioengine=psync \
+		--rw=randread \
+		--name "$NAME" \
+		--filename "$1" \
+		--output-format=json \
+		"--bs=$BLOCKSIZE" \
+		--output "$FILENAME"
+done
